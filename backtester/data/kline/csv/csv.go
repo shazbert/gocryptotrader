ddfac44e7de406f1e659f70e61e94ce4a070beae
Ryan O'Hara-Reid <ryan.oharareid@thrasher.io>
2023-02-15 15:16:36 +1100

    backtester/csv: improved data handling from csv data source

--- backtester/data/kline/csv/csv.go
+++ backtester/data/kline/csv/csv.go
@@ -4,10 +4,8 @@ import (
 	"encoding/csv"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/thrasher-corp/gocryptotrader/backtester/common"
@@ -16,150 +14,138 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	gctkline "github.com/thrasher-corp/gocryptotrader/exchanges/kline"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/order"
-	"github.com/thrasher-corp/gocryptotrader/exchanges/trade"
-	"github.com/thrasher-corp/gocryptotrader/log"
 )
 
-var errNoUSDData = errors.New("could not retrieve USD CSV candle data")
+var (
+	errNoUSDData = errors.New("could not retrieve USD CSV candle data")
+	errNoCandles = errors.New("no candles from data type")
+)
 
-// LoadData is a basic csv reader which converts the found CSV file into a kline item
-func LoadData(dataType int64, filepath, exchangeName string, interval time.Duration, fPair currency.Pair, a asset.Item, isUSDTrackingPair bool) (*kline.DataFromKline, error) {
-	resp := kline.NewDataFromKline()
+// LoadData is a basic csv reader which converts the found CSV file into a kline
+// item. WARNING: When fetching candles with a misconfigured interval this may
+// add padding (empty candles) between candles for that time period.
+func LoadData(dataType int64, filepath, exchangeName string, in gctkline.Interval, pair currency.Pair, a asset.Item, isUSDTrackingPair bool) (*kline.DataFromKline, error) {
 	csvFile, err := os.Open(filepath)
 	if err != nil {
 		return nil, err
 	}
 
-	defer func() {
-		err = csvFile.Close()
-		if err != nil {
-			log.Errorln(common.Data, err)
-		}
-	}()
+	records, err := csv.NewReader(csvFile).ReadAll()
 
-	csvData := csv.NewReader(csvFile)
+	closeErr := csvFile.Close()
+	if closeErr != nil {
+		return nil, closeErr
+	}
 
+	if err != nil {
+		return nil, err
+	}
+
+	var timeSeries []gctkline.Candle
 	switch dataType {
 	case common.DataCandle:
-		candles := gctkline.Item{
-			Exchange: exchangeName,
-			Pair:     fPair,
-			Asset:    a,
-			Interval: gctkline.Interval(interval),
+		timeSeries = make([]gctkline.Candle, len(records))
+		for x := range records {
+			var seconds int64
+			seconds, err = strconv.ParseInt(records[x][0], 10, 64)
+			if err != nil {
+				return nil, err
+			}
+
+			timeSeries[x].Time = time.Unix(seconds, 0).UTC()
+			if timeSeries[x].Time.Unix() == 0 {
+				return nil, fmt.Errorf("invalid timestamp received on row %v", records[x][0])
+			}
+
+			timeSeries[x].Volume, err = strconv.ParseFloat(records[x][1], 64)
+			if err != nil {
+				return nil, fmt.Errorf("could not process candle volume %v %w", records[x][1], err)
+			}
+
+			timeSeries[x].Open, err = strconv.ParseFloat(records[x][2], 64)
+			if err != nil {
+				return nil, fmt.Errorf("could not process candle volume %v %w", records[x][2], err)
+			}
+
+			timeSeries[x].High, err = strconv.ParseFloat(records[x][3], 64)
+			if err != nil {
+				return nil, fmt.Errorf("could not process candle high %v %w", records[x][3], err)
+			}
+
+			timeSeries[x].Low, err = strconv.ParseFloat(records[x][4], 64)
+			if err != nil {
+				return nil, fmt.Errorf("could not process candle low %v %w", records[x][4], err)
+			}
+
+			timeSeries[x].Close, err = strconv.ParseFloat(records[x][5], 64)
+			if err != nil {
+				return nil, fmt.Errorf("could not process candle close %v %w", records[x][5], err)
+			}
 		}
-
-		for {
-			row, errCSV := csvData.Read()
-			if errCSV != nil {
-				if errCSV == io.EOF {
-					break
-				}
-				return nil, fmt.Errorf("could not read csv data for %v %v %v, %v", exchangeName, a, fPair, errCSV)
-			}
-
-			candle := gctkline.Candle{}
-			v, errParse := strconv.ParseInt(row[0], 10, 32)
-			if errParse != nil {
-				return nil, errParse
-			}
-			candle.Time = time.Unix(v, 0).UTC()
-			if candle.Time.IsZero() {
-				err = fmt.Errorf("invalid timestamp received on row %v %v", row[0], err)
-				break
-			}
-
-			candle.Volume, err = strconv.ParseFloat(row[1], 64)
-			if err != nil {
-				err = fmt.Errorf("could not process candle volume %v %v", row[1], err)
-				break
-			}
-
-			candle.Open, err = strconv.ParseFloat(row[2], 64)
-			if err != nil {
-				err = fmt.Errorf("could not process candle volume %v %v", row[2], err)
-				break
-			}
-
-			candle.High, err = strconv.ParseFloat(row[3], 64)
-			if err != nil {
-				err = fmt.Errorf("could not process candle high %v %v", row[3], err)
-				break
-			}
-
-			candle.Low, err = strconv.ParseFloat(row[4], 64)
-			if err != nil {
-				err = fmt.Errorf("could not process candle low %v %v", row[4], err)
-				break
-			}
-
-			candle.Close, err = strconv.ParseFloat(row[5], 64)
-			if err != nil {
-				err = fmt.Errorf("could not process candle close %v %v", row[5], err)
-				break
-			}
-
-			candles.Candles = append(candles.Candles, candle)
-		}
-		if err != nil {
-			return nil, fmt.Errorf("could not read csv candle data for %v %v %v, %v", exchangeName, a, fPair, err)
-		}
-		resp.Item = &candles
 	case common.DataTrade:
-		var trades []trade.Data
-		for {
-			row, errCSV := csvData.Read()
-			if errCSV != nil {
-				if errCSV == io.EOF {
-					break
-				}
-				return nil, errCSV
-			}
-
-			t := trade.Data{}
-			v, errParse := strconv.ParseInt(row[0], 10, 32)
-			if errParse != nil {
-				return nil, errParse
-			}
-			t.Timestamp = time.Unix(v, 0).UTC()
-			if t.Timestamp.IsZero() {
-				err = fmt.Errorf("invalid timestamp received on row %v", row)
-				break
-			}
-
-			t.Price, err = strconv.ParseFloat(row[1], 64)
+		trades := make([]order.TradeHistory, len(records))
+		for x := range records {
+			var seconds int64
+			seconds, err = strconv.ParseInt(records[x][0], 10, 64)
 			if err != nil {
-				err = fmt.Errorf("could not process trade price %v, %v", row[1], err)
-				break
+				return nil, err
+			}
+			trades[x].Timestamp = time.Unix(seconds, 0).UTC()
+			if trades[x].Timestamp.Unix() == 0 {
+				return nil, fmt.Errorf("invalid timestamp received on row %v", records[x][0])
 			}
 
-			t.Amount, err = strconv.ParseFloat(row[2], 64)
+			trades[x].Price, err = strconv.ParseFloat(records[x][1], 64)
 			if err != nil {
-				err = fmt.Errorf("could not process trade amount %v, %v", row[2], err)
-				break
+				return nil, fmt.Errorf("could not process trade price %v, %w", records[x][1], err)
 			}
 
-			t.Side, err = order.StringToOrderSide(row[3])
+			trades[x].Amount, err = strconv.ParseFloat(records[x][2], 64)
 			if err != nil {
-				err = fmt.Errorf("could not process trade side %v, %v", row[3], err)
-				break
+				return nil, fmt.Errorf("could not process trade amount %v, %w", records[x][2], err)
 			}
 
-			trades = append(trades, t)
+			trades[x].Side, err = order.StringToOrderSide(records[x][3])
+			if err != nil {
+				return nil, fmt.Errorf("could not process trade side %v, %w", records[x][3], err)
+			}
 		}
-		resp.Item, err = trade.ConvertTradesToCandles(gctkline.Interval(interval), trades...)
+
+		var temp *gctkline.Item
+		temp, err = gctkline.CreateKline(trades, in, pair, a, exchangeName)
 		if err != nil {
-			return nil, fmt.Errorf("could not read csv trade data for %v %v %v, %v", exchangeName, a, fPair, err)
+			return nil, err
 		}
+		timeSeries = temp.Candles
 	default:
 		if isUSDTrackingPair {
-			return nil, fmt.Errorf("%w for %v %v %v. Please add USD pair data to your CSV or set `disable-usd-tracking` to `true` in your config. %v", errNoUSDData, exchangeName, a, fPair, err)
+			return nil, fmt.Errorf("%w for %v %v %v. Please add USD pair data to your CSV or set `disable-usd-tracking` to `true` in your config. %v",
+				errNoUSDData, exchangeName, a, pair, err)
 		}
-		return nil, fmt.Errorf("could not process csv data for %v %v %v, %w", exchangeName, a, fPair, common.ErrInvalidDataType)
+		return nil, fmt.Errorf("could not process csv data for %v %v %v, %w",
+			exchangeName, a, pair, common.ErrInvalidDataType)
 	}
-	resp.Item.Exchange = strings.ToLower(exchangeName)
-	resp.Item.Pair = fPair
-	resp.Item.Asset = a
-	resp.Item.Interval = gctkline.Interval(interval)
 
-	return resp, nil
+	if len(timeSeries) == 0 {
+		return nil, errNoCandles
+	}
+
+	// NOTE: This infers the start and end date specifically from the stored
+	// data.
+	start := timeSeries[0].Time
+	end := timeSeries[len(timeSeries)-1].Time.Add(in.Duration())
+
+	klineRequest, err := gctkline.CreateKlineRequest(exchangeName, pair, pair, a, in, in, start, end)
+	if err != nil {
+		return nil, err
+	}
+
+	// NOTE: This is a process to correctly manage, filter and structure the
+	// data and to ensure it is not invalid.
+	klineItem, err := klineRequest.ProcessResponse(timeSeries)
+	if err != nil {
+		return nil, err
+	}
+
+	return kline.NewDataFromKline(klineItem, start, end)
 }
--- backtester/data/kline/csv/csv_test.go
+++ backtester/data/kline/csv/csv_test.go
@@ -11,53 +11,70 @@ import (
 	gctkline "github.com/thrasher-corp/gocryptotrader/exchanges/kline"
 )
 
-const testExchange = "binance"
+const exch = "binance"
+
+var pair = currency.NewPair(currency.BTC, currency.USDT)
 
 func TestLoadDataCandles(t *testing.T) {
-	exch := testExchange
-	a := asset.Spot
-	p := currency.NewPair(currency.BTC, currency.USDT)
-	_, err := LoadData(
-		common.DataCandle,
+	data, err := LoadData(common.DataCandle,
 		filepath.Join("..", "..", "..", "..", "testdata", "binance_BTCUSDT_24h_2019_01_01_2020_01_01.csv"),
 		exch,
-		gctkline.FifteenMin.Duration(),
-		p,
-		a,
+		gctkline.OneDay,
+		pair,
+		asset.Spot,
 		false)
 	if !errors.Is(err, nil) {
 		t.Errorf("received: %v, expected: %v", err, nil)
+	}
+
+	if len(data.Item.Candles) != 365 {
+		t.Fatalf("received: '%v' but expected: '%v'", len(data.Item.Candles), 365)
+	}
+
+	stream, err := data.GetStream()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if len(stream) != 365 {
+		t.Fatalf("received: '%v' but expected: '%v'", len(stream), 365)
 	}
 }
 
 func TestLoadDataTrades(t *testing.T) {
-	exch := testExchange
-	a := asset.Spot
-	p := currency.NewPair(currency.BTC, currency.USDT)
-	_, err := LoadData(
-		common.DataTrade,
+	data, err := LoadData(common.DataTrade,
 		filepath.Join("..", "..", "..", "..", "testdata", "binance_BTCUSDT_24h-trades_2020_11_16.csv"),
 		exch,
-		gctkline.FifteenMin.Duration(),
-		p,
-		a,
+		gctkline.OneMin,
+		pair,
+		asset.Spot,
 		false)
 	if !errors.Is(err, nil) {
 		t.Errorf("received: %v, expected: %v", err, nil)
 	}
+
+	if len(data.Item.Candles) != 4 {
+		t.Fatalf("received: '%v' but expected: '%v'", len(data.Item.Candles), 4)
+	}
+
+	stream, err := data.GetStream()
+	if !errors.Is(err, nil) {
+		t.Fatalf("received: '%v' but expected: '%v'", err, nil)
+	}
+
+	if len(stream) != 4 {
+		t.Fatalf("received: '%v' but expected: '%v'", len(stream), 4)
+	}
 }
 
 func TestLoadDataInvalid(t *testing.T) {
-	exch := testExchange
-	a := asset.Spot
-	p := currency.NewPair(currency.BTC, currency.USDT)
 	_, err := LoadData(
 		-1,
 		filepath.Join("..", "..", "..", "..", "testdata", "binance_BTCUSDT_24h-trades_2020_11_16.csv"),
 		exch,
-		gctkline.FifteenMin.Duration(),
-		p,
-		a,
+		gctkline.FifteenMin,
+		pair,
+		asset.Spot,
 		false)
 	if !errors.Is(err, common.ErrInvalidDataType) {
 		t.Errorf("received: %v, expected: %v", err, common.ErrInvalidDataType)
@@ -67,9 +84,9 @@ func TestLoadDataInvalid(t *testing.T) {
 		-1,
 		filepath.Join("..", "..", "..", "..", "testdata", "binance_BTCUSDT_24h-trades_2020_11_16.csv"),
 		exch,
-		gctkline.FifteenMin.Duration(),
-		p,
-		a,
+		gctkline.FifteenMin,
+		pair,
+		asset.Spot,
 		true)
 	if !errors.Is(err, errNoUSDData) {
 		t.Errorf("received: %v, expected: %v", err, errNoUSDData)
